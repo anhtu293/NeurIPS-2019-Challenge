caa2ac88933cb033ce88c088b2991956e5e7a5ce
Yunfei <yunfei@yunfeis-macbook.home>
2019-11-01 19:57:03 +0100

    clear

--- manipulate.py
+++ manipulate.py
@@ -10,11 +10,16 @@ observation = env.reset(project=True, seed=None, init_pose=None, obs_as_dict=Tru
 def init():
     tools = Tools()
     for i in range(200):
-        observation, reward, done, info = env.step(tools.active_one_muscle("rect_fem", "r", 1))
+        observation, reward, done, info = env.step(tools.active_one_muscle("iliopsoas", "r", 1))
         if i == 20:
+            state_desc = env.get_state_desc()
+            print(state_desc["body_pos"]["toes_r"][0:2])
+            print(state_desc["body_pos"]["talus_l"][0:2])
+            print(state_desc["misc"]["mass_center_pos"])
             input("Press Enter to continue...")
             print(reward)
 
 
 if __name__ == '__main__':
    init()
+
