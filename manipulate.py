64154058301f2214384d803693420f7e789af6ae
Yunfei <yunfei@yunfeis-macbook.home>
2019-11-21 00:35:31 +0100

    direction

--- manipulate.py
+++ manipulate.py
@@ -9,13 +9,16 @@ observation = env.reset(project=True, seed=None, init_pose=None, obs_as_dict=Tru
 
 def init():
     tools = Tools()
+
     for i in range(200):
         observation, reward, done, info = env.step(tools.active_one_muscle("iliopsoas", "r", 1))
+
         if i == 20:
             state_desc = env.get_state_desc()
-            print(state_desc["body_pos"]["toes_r"][0:2])
+            print(type(state_desc["body_pos"]["toes_r"][0:2]))
             print(state_desc["body_pos"]["talus_l"][0:2])
             print(state_desc["misc"]["mass_center_pos"])
+            print(state_desc["body_pos_rot"])
             input("Press Enter to continue...")
             print(reward)
 
--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import math
 
 # Tools for training
 # 1) direction():
@@ -31,9 +31,28 @@ class Tools:
                     return
             pos += 1
         return activations
-
-
+# Counterclockwise
+    def get_direction(self, toes_l, toes_r, mass_center_pos):
+        l = np.asarray(toes_l)
+        r = np.asarray(toes_r)
+        mp = np.asarray((l + r)/2)
+        mass_center_pos = np.asarray(mass_center_pos)
+        l_to_r = r - l
+        rot = np.pi/2
+        m_t = np.zeros((2, 2))
+        m_t[0][0] = np.cos(rot)
+        m_t[0][1] = -np.sin(rot)
+        m_t[1][0] = np.sin(rot)
+        m_t[1][1] = -np.cos(rot)
+        straight_direction = np.dot(m_t, l_to_r)
+        body_direction = mass_center_pos - mp
+        theta = math.atan2(body_direction[1], body_direction[0]) - \
+                math.atan2(straight_direction[1], straight_direction[0])
+        theta = theta * 180 / np.pi
+        return theta
 
 if __name__ == '__main__':
     tools = Tools()
-    print(tools.active_one_muscle("rect_fem", "r", 1))
+    #print(tools.active_one_muscle("rect_fem", "r", 1))
+    a = math.sqrt(3)
+    print(tools.get_direction([-1, 0], [1, 0 ], [a, -1]))
